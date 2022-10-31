5c1bc23d2b99286daee90c6f9b4b14f51c17fef9
Yiren Zhou <yiz112@ucsd.edu>
2022-10-30 21:27:19 -0700

    expose1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,27 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  const page = document.querySelector("html");
+  page.addEventListener('load', function() {
+    const listVoice = SpeechSynthesis.getVoices();
+  const changeVoice = document.getElementById("voice-select");
+  var option;
+  for (let i = 0; i < listVoice.length; i++) { 
+    option = document.createElement("option");
+    option.text = listVoice[i].name;
+    changeVoice.add(option);
+  }
+});
+
+const button = document.querySelector("button");
+
+button.addEventListener('click', function() {
+  if (changeImage.value == "party-horn") {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
+  }
+
+  soundFile.play();
+});
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,7 +7,7 @@ function init() {
   const soundFile = document.querySelector(".hidden");
   changeImage.addEventListener('change', function() {
     const result = document.querySelector("img");
-    result.src = "assets/images/" + changeImage.value + ".png";
+    result.src = "assets/images/" + changeImage.value + ".svg";
     
     soundFile.src = "assets/audio" + changeImage.value + ".mp3";
 });
@@ -17,13 +17,13 @@ const changeVolume = document.getElementById("volume-controls");
 changeVolume.addEventListener('change', function() {
   const result = document.querySelectorAll("img")[1];
   if (changeVolume.value == 0)
-    result.src = "assets/icons/volume-level-0.png";
+    result.src = "assets/icons/volume-level-0.svg";
   else if (changeVolume.value < 33)
-    result.src = "assets/icons/volume-level-1.png";
+    result.src = "assets/icons/volume-level-1.svg";
   else if (changeVolume.value < 67)
-    result.src = "assets/icons/volume-level-2.png";
+    result.src = "assets/icons/volume-level-2.svg";
   else
-    result.src = "assets/icons/volume-level-3.png";
+    result.src = "assets/icons/volume-level-3.svg";
   soundFile.volume = changeVolume.value/100;
 });
 
