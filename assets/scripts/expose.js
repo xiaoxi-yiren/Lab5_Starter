9770b42ed599cd3bff8d905fbcf0b28aa9e5d741
Yiren Zhou <yiz112@ucsd.edu>
2022-10-30 22:04:02 -0700

    expose3

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -14,17 +14,17 @@ function init() {
 
 const changeVolume = document.getElementById("volume-controls");
 
-changeVolume.addEventListener('change', function() {
+changeVolume.addEventListener('change', (event) => {
   const result = document.querySelectorAll("img")[1];
-  if (changeVolume.value == 0)
+  if (event.target.value == 0)
     result.src = "assets/icons/volume-level-0.svg";
-  else if (changeVolume.value < 33)
+  else if (event.target.value < 33)
     result.src = "assets/icons/volume-level-1.svg";
-  else if (changeVolume.value < 67)
+  else if (event.target.value < 67)
     result.src = "assets/icons/volume-level-2.svg";
   else
     result.src = "assets/icons/volume-level-3.svg";
-  soundFile.volume = changeVolume.value/100;
+  soundFile.volume = changeVolume.value/100.0;
 });
 
 const button = document.querySelector("button");
