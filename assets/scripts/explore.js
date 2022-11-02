434ebe82df3d1b9e3a0ef22db699d6255aa0a565
Yiren Zhou <yiz112@ucsd.edu>
2022-11-01 23:26:36 -0700

    final

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,30 +3,42 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-
-  const page = document.querySelector("body");
-  const listVoice = SpeechSynthesis.getVoices();
-  page.addEventListener('load', function() {
+  var listVoice = [];
+  const changeVoice = document.getElementById("voice-select");
+  speechSynthesis.onvoiceschanged = () => {
+  //const page = document.querySelector("body");
+  listVoice = speechSynthesis.getVoices();
+  console.log(listVoice);
+  //page.addEventListener('load', function() {
     
-    const changeVoice = document.getElementById("voice-select");
-    var option;
-    for (let i = 0; i < listVoice.length; i++) { 
-      option = document.createElement("option");
-      option.textContent = `${listVoice[i].name} (${listVoice[i].lang})`;
-      changeVoice.appendChild(option);
+  
+  var option;
+  
+  for (let i = 0; i < listVoice.length; i++) { 
+    option = document.createElement("option");
+    option.textContent = `${listVoice[i].name} (${listVoice[i].lang})`;
+    changeVoice.appendChild(option);
+  }}
+  //});
+
+  const button = document.querySelector("button");
+  const img = document.querySelector("img");
+  button.addEventListener('click', function() {
+    const synth = window.speechSynthesis;
+    const textArea = document.getElementById("text-to-speak");
+    const utterThis = new SpeechSynthesisUtterance(textArea.value);
+    //var listVoice = speechSynthesis.getVoices();
+    console.log(listVoice);
+    for (let j = 0; j < listVoice.length; j++) {
+      if (listVoice[j].name == changeVoice.value)
+        utterThis.voice = listVoice[j];
     }
-});
+    synth.speak(utterThis);
+    
+    img.src = "assets/images/smiling-open.png"
+    utterThis.addEventListener('end', () => {
+      img.setAttribute('src', 'assets/images/smiling.png')
+    });
+  });
 
-const button = document.querySelector("button");
-
-button.addEventListener('click', function() {
-  const synth = window.speechSynthesis;
-  const textArea = document.getElementById("text-to-speak");
-  const utterThis = new SpeechSynthesisUtterance(textArea.value);
-  for (let j = 0; j < listVoice.length; j++) {
-    if (listVoice[j].name == changeVoice.value)
-      utterThis.voice = listVoice[j];
-  }
-  synth.speak(utterThis);
-});
 }
